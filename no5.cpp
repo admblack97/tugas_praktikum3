8306bf03d1f50780b53dad87d27eeaa9f3fda534
admblack97 <admblack97@gmail.com>
2017-11-06 07:51:10 +0800

    float nya ada yg kd tepakai, ulun buang ja hahahahaha

--- no5.cpp
+++ no5.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main () {
-	float berat, kurus_karing, normal, lamak_banar;
+	float berat;
 	int tinggi;
 	
 	cout << "Masukkan tinggi badan dalam cm : ";
@@ -12,16 +12,14 @@ int main () {
 	cin >> berat;	
 	
 	if (berat<tinggi/2.5) {
-		cout << "Hasil : Kurus Banar" << endl;
+		cout << "Hasil : Kurus banar sanak ai lamaki lg" << endl;
 	}
 	else if  (tinggi/2.5 <= berat <= tinggi/2.3) {
-		cout <<  "Hasil : Kelamakan" << endl; 
+		cout <<  "Hasil : Ideal haja, sadang dah awak pian" << endl; 
 	}
 	
-//	else if (tinggi/2.3 < berat)
-//		cout << "Hasil : "
-	
-		
+	else if (tinggi/2.3 < berat)
+		cout << "Hasil : Kelamakan pulang sanak ai, bekurus gin lg" << endl;
 	
 }
 
