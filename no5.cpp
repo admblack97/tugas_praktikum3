d98b8d943a0bf058210b7ae6d25ee4c6f1ee8cb6
admblack97 <admblack97@gmail.com>
2017-11-06 07:24:54 +0800

    input tinggi berat badan berhasil

---
+++ no5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	float berat, kurus, normal, gemuk;
+	int tinggi;
+	
+	cout << "Masukkan tinggi badan dalam cm : ";
+	cin >> tinggi;
+	
+	cout << "Masukkan berat badan dalam kg : ";
+	cin >> berat;	
+	
+}
+
