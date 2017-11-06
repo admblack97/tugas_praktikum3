7955b575fcc726d5546ae0b02341504f254ee84d
admblack97 <admblack97@gmail.com>
2017-11-06 22:40:08 +0800

    yesss berhasil berhasil :+1: :100: :yum:

--- no2.cpp
+++ no2.cpp
@@ -5,28 +5,36 @@ using namespace std;
 
 int main () {
 char bidang;
-double P,p,S,s,l,a,t,L;
+double p,l,a,t,L;
 
 cout << "Pilih Bidang : ";
 cin >> bidang;
 
-	if ((bidang==P) && (bidang==p)) {
-		cout << "Mengitung luas persegi panjang \n";	
-		cout << "Masukkan panjangnya : \n";
+	if (bidang == 'P' || bidang == 'p') {
+		cout << "+================================+ \n";	
+		cout << "   Mengitung luas persegi panjang \n";
+		cout << "+================================+ \n";	
+		cout << "   Masukkan panjangnya : ";
 		cin >> p;
-		cout << "Masukkan lebarnya   : \n";
+		cout << "   Masukkan lebarnya   : ";
 		cin >> l;
 		L = p*l;
-		cout << "Luas persegi panjang : "<< L<<endl;
+		cout << "+================================+ \n";
+		cout << "   Luas persegi panjang : "<< L<<endl;
+		cout << "+================================+ \n";
 	}
-	else if ((bidang==S) && (bidang==s)) {
-		cout << "Mengitung luas segitiga \n";	
-		cout << "Masukkan alasnya : \n";
+	else if (bidang == 'S' || bidang == 's') {
+		cout << "+================================+ \n";
+		cout << "   Mengitung luas segitiga \n";
+		cout << "+================================+ \n";	
+		cout << "   Masukkan alasnya   : ";
 		cin >> a;
-		cout << "Masukkan tingginya   : \n";
+		cout << "   Masukkan tingginya : ";
 		cin >> t;
 		L = 0.5*a*t;
-		cout << "Luas persegi panjang : "<< L<<endl;
+		cout << "+================================+ \n";
+		cout << "   Luas persegi panjang : "<< L<<endl;
+		cout << "+================================+ \n";
 	}	
 	else{
 		cout<<"Maaf, Anda Salah Memilih"<<endl;
