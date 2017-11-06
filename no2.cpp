c1a94f72cf9179da1a2497b9738643abe8549cf1
admblack97 <admblack97@gmail.com>
2017-11-06 12:31:10 +0800

    input bidang berhasil

--- no2.cpp
+++ no2.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include <stdio.h>
 #include <conio.h>
+using namespace std;
 
-char bidang;
-double p,l,a,t,L;
+int main () {
+	char bidang;
+	double p,l,a,t,L;
 
-cout << "Pilih Bidang : ";<<endl;
+cout << "Pilih Bidang : "<<endl;
 cin >> bidang;
-	
+
 }
+	
+
 
 
