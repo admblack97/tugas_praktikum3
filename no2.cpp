9b9584faf42c6a192a76e0b0c73d61191906d6af
admblack97 <admblack97@gmail.com>
2017-11-06 12:35:10 +0800

    variabel P ny kd ingat membuat wkwkwk

--- no2.cpp
+++ no2.cpp
@@ -4,14 +4,25 @@
 using namespace std;
 
 int main () {
-	char bidang;
-	double p,l,a,t,L;
+char bidang;
+double p,l,a,t,L;
 
 cout << "Pilih Bidang : "<<endl;
 cin >> bidang;
 
-}
+	if (bidang == P)  {
+		cout << "Mengitung luas persegi panjang \n";
+		
+		cout << "Masukkan panjangnya : \n";
+		cin >> p;
+		cout << "Masukkan lebarnya   : \n";
+		cin >> l;
+		L = p*l;
+		cout << "Luas persegi panjang : "<< L<<endl;
+		
+	}	
 	
+}
 
 
 
