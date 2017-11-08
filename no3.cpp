a2a0401c5efc0afbb6a067c3dd31b1f2e7a10e6e
admblack97 <admblack97@gmail.com>
2017-11-08 22:44:07 +0800

    ini berhasil jua, gaji perminggu (variabel nya ulun tambahi, else nya jua) :+1: :+1:

--- no3.cpp
+++ no3.cpp
@@ -4,18 +4,24 @@
 using namespace std;
 
 int main () {
-	int gp,jk,lembur,gt,pajak;
+	int gp,jk,lembur,gt,gb,pajak;
 	
 	cout << "Masukkan gaji pegawai : ";
 	cin >> gp;
 	cout << "Masukkan jam kerja    : ";
 	cin >> jk;
 	
-	lembur= jk - 40;
-	gt = (lembur*1.5+40)+gp;
-	pajak=gt*0.15;
-	gt=gt-pajak;
+	if(jk>40){
+		lembur= (jk - 40)*(gp/40);
+		gt = (lembur*1.5)+gp;
+		pajak=gt*0.15;
+		gb=gt-pajak;
+	}
+	else {
+		pajak=gp*0.15;
+		gb=gp-pajak;
+	}
 	
-	cout<<"Gaji bersih pegawai : "<< gt<<endl;
+	cout<<"Gaji bersih pegawai : "<< gb<<endl;
 	
 }
